750e7fcd981a9f809fd4ff9c6cf72493b44b6852
Brianoxo <brianbabo26@gmail.com>
2026-09-16 11:25:56 +0800

    feat_bgcolor_blue

--- git_practice01.js
+++ git_practice01.js
@@ -3,7 +3,7 @@ tailwind.config = {
                 extend: {
                     colors: {
                         dungeon: {
-                            950: '#bd297fff',
+                            950: '#3f03c9ff',
                             900: '#23b8a4ff',
                             800: '#1a8b3cff',
                         }
